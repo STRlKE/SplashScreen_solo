2fcf67be56e42fd438255cac9f4aa72444aa05f6
Kwan-123 <75381919+Kwan-123@users.noreply.github.com>
2022-03-28 13:57:00 +0900

    Initial Commit

---
+++ Shared/SplashScreen_soloApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreen_soloApp.swift
+//  Shared
+//
+//  Created by 하명관 on 2022/03/28.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreen_soloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
